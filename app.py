0aeee51147db33abc731ae557b37c36c1042c3cd
Carlos Arturo Gómez Jiménez <110650726+cargeo95@users.noreply.github.com>
2024-05-24 13:21:59 -0500

    Update app3.py

--- app.py
+++ app.py
@@ -9,4 +9,5 @@ server = app.server
 app.layout = layout
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=True, host='0.0.0.0', port=port)
